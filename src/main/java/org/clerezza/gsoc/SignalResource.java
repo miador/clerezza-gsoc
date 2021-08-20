f48b7c1c990f0323977fae4b006af609e9016b50
miador <yusufkaradag1402@gmail.com>
2021-08-20 15:56:21 +0200

    resources of project

--- src/main/java/org/clerezza/gsoc/SignalResource.java
+++ src/main/java/org/clerezza/gsoc/SignalResource.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.clerezza.signal.file.FileOperations;
 import org.apache.clerezza.signal.graph.SignalGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.io.IOException;
 
 @Path( "/signal/messages" )
@@ -14,12 +17,13 @@ import java.io.IOException;
 @Consumes( MediaType.APPLICATION_JSON )
 public class SignalResource {
 
+    private static final Logger logger = LoggerFactory.getLogger( SignalResource.class );
     public static String filename = System.getProperty( "user.dir" ) + "/signal.ttl";
 
     @POST
     public void saveMessage( String data ) throws IOException {
         var json = new ObjectMapper().readValue( data, ObjectNode.class );
-        FileOperations.createFileIfNotExist( filename );
+        createFileIfNotExist( filename );
         SignalGraph.buildGraph( json, filename );
 
     }
@@ -29,4 +33,21 @@ public class SignalResource {
         return FileOperations.printGraph( filename );
     }
 
+    /**
+     *
+     * @param filename, with the given filename a new file will be created if file doesn't exist.
+     *                  Will do nothing if file already exists.
+     */
+    public static void createFileIfNotExist( String filename ) {
+        try {
+            File file = new File( filename );
+            if ( file.createNewFile() ) {
+                logger.info( "File created at: " + "\"" + filename + "\"" );
+            } else {
+                logger.info( "Existing file at the directory: " + "\"" + filename + "\"" + " will be overwritten by the serializer" );
+            }
+        } catch ( Exception e ) {
+            System.out.println( e.getLocalizedMessage() );
+        }
+    }
 }
--- src/test/java/org/clerezza/gsoc/NativeResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.clerezza.gsoc;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeResourceIT extends ResourceTest {
-
-    // Execute the same tests but in native mode.
-}
